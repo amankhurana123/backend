4c20da1b788010a6201899e309ec39c52a798789
rashi <rashi.jain@daffodilsw.com>
2018-06-16 18:51:40 +0530

    like comment

--- api/showPostapi.js
+++ api/showPostapi.js
@@ -2,16 +2,24 @@ const showPosts = require("../schema/showPost");
 module.exports = {
   addLikeAndComment: data => {
     return new Promise((resolve, reject) => {
-      // showPosts.find((error, result) => {
-      //   resolve(result);
-      // });
       showPosts
-        .findByIdAndUpdate(
-          { postId: data._id },
-          { $set: { like: data.like }, $push: { comment: data.comment } }
-        )
+
+        .create({ data })
         .then(result => {
+          console.log("result", result);
           resolve(result);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  getpost: data => {
+    return new Promise((resolve, reject) => {
+      showPosts
+        .find({ userId: data.userId })
+        .then(response => {
+          resolve(response);
         })
         .catch(error => {
           reject(error);
--- router/routerUpload.js
+++ router/routerUpload.js
@@ -2,14 +2,31 @@ const express = require("express");
 const router = express.Router();
 const showPost = require("../api/showPostapi");
 router.post("/post", async (req, res) => {
-  console.log("req.body", req.body);
+  const postData = req.body;
+  console.log("postData", postData);
   try {
-    const post = await showPost.addLikeAndComment(req.body);
+    const post = await showPost.addLikeAndComment(postData);
     if (post.length) {
-      console.log(post);
+      console.log("post", post);
       res.send("'response send");
     }
   } catch (errot) {
+    console.log("error", error);
+  }
+});
+router.get("/show", async (req, res) => {
+  console.log("req.params in show post", JSON.parse(req.query.params));
+  try {
+    const data = await showPost.getpost(JSON.parse(req.query.params));
+    if (data.length) {
+      console.log(data);
+      res.send(data);
+    } else {
+      console.log("data not found", data);
+
+      res.send("data not found");
+    }
+  } catch (error) {
     console.log("error", error);
   }
 });
